fd73969ac94bb2a6c21517c90e6c3730d23383c1
Kai INUI <lied.der.optik@gmail.com>
2014-07-26 21:28:07 +0900

    pull_sync methods

--- lib/aquasync/collection.rb
+++ lib/aquasync/collection.rb
@@ -9,11 +9,39 @@ class Aquasync::Collection
   end
 
   def pull_sync
+    local_store = Aquasync::LocalStore.instance
+    latest_ust = local_store.latest_ust
+    device_token = local_store.device_token
 
+    deltas = Aquasync::MasterCollection.instance.retrieve_deltas(device_token, latest_ust)
+    update_records_by_deltas(deltas)
   end
 
   def push_sync
 
+  end
+
+  def retrieve_deltas(device_token, latest_ust)
+    @collection.select {|r| r.ust > latest_ust }.select {|r| r.device_token != device_token}
+  end
+
+  def update_records_by_deltas(deltas)
+    deltas.each do |delta|
+      record = find(delta.gid)
+      if record
+        record.resolve_conflict(delta)
+      else
+        create_record_from_delta(delta)
+      end
+    end
+  end
+
+  def create_record_from_delta(delta)
+    @collection.push delta
+  end
+
+  def find(gid)
+    @collection.select {|r| r.gid == gid}.first
   end
 
   def dirty_resources
