84cf9466a199f312c2fe532535f6d25ddea9f9b5
Kai INUI <lied.der.optik@gmail.com>
2014-07-26 19:12:00 +0900

    refactor Aquasync::Model

--- lib/aquasync/model.rb
+++ lib/aquasync/model.rb
@@ -5,6 +5,32 @@ class Aquasync::Model
 
   def initialize
     self.gid = (UUID.new).generate
-    self.local_timestamp = Time.now.getutc.to_i
+    after_create_or_update
+  end
+
+  def update_attribute(key, value)
+    self.send("#{key}=", value)
+    after_create_or_update
+  end
+
+  def after_create_or_update
+    dirty!
+    set_timestamp
+  end
+
+  def set_timestamp
+    self.local_timestamp = new_timestamp
+  end
+
+  def dirty?
+    dirty
+  end
+
+  def dirty!
+    dirty = true
+  end
+
+  def new_timestamp
+    Time.now.getutc.to_i
   end
 end
